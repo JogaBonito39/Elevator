91f90be4c56e0274489d3a17b221fd7d4d749382
JogaBonito39 <109993794+JogaBonito39@users.noreply.github.com>
2024-10-07 22:24:03 -0400

    Update Elevator.java

--- Elevator.java
+++ Elevator.java
@@ -13,28 +13,63 @@ public class Elevator {
         int requests = Integer.parseInt(args[1]);
         int restrictedFloors = Integer.parseInt(args[2]);
         int passcode;
+        int elevator1 = 1;
+        int elevator2 = 1;
+        
+
+        int tempRequests1 = requests;
+        int tempRequests2 = requests;
+        int count = 0;
+        int i = 0;
+        int val = 0;
+        int newMinVal=0;
+        int[] reqArr = new int[10];
+        boolean boolCont = false;
         if (restrictedFloors > 0){
+            boolCont = true;
             passcode = Integer.parseInt(args[3]);
+            
+    
+            while (tempRequests2>0){
+                tempRequests2= tempRequests2/10;
+                count++;
+            }
+    
+            reqArr = new int[count];
+            
+            while (tempRequests1>0){
+                val = tempRequests1%10;
+                reqArr[i] = val;
+                tempRequests1 = tempRequests1/10;
+                i++;
+            }
+            
+            for (int a=0; a<reqArr.length; a++){
+                for(int b=0; b<reqArr.length; b++){
+                    if (reqArr[b]>reqArr[a]){
+                        int temp = reqArr[a];
+                        reqArr[a] = reqArr[b];
+                        reqArr[b] = temp;
+                    }
+                }
+            }
+            
+            newMinVal = reqArr[reqArr.length-restrictedFloors];
+            
+            i = 1;
         }
         else{
             passcode = 0;
+            boolCont = false;
         }
             
-        int tempRequests = requests;
-        int elevator1 = 1;
-        int elevator2 = 1;
-        int count = 0;
-        while (tempRequests > 0){
-            tempRequests/=10;
-            count++;
-        }
 
-    
         while (requests > 0){
             int curReq = requests%10;
-            if (count == restrictedFloors && restrictedFloors > 0){
+            if (curReq >= newMinVal && boolCont){
                 String access = "Denied";
-                if (passcode % floors == curReq || (passcode%floors == 0 && count == restrictedFloors)){
+                i++;
+                if (passcode % floors == curReq || passcode%floors == 0 && curReq == reqArr[reqArr.length-1]){
                     access = "Granted";
                 }
                 else{
@@ -48,7 +83,7 @@ public class Elevator {
                     elevator1 = curReq;
 
                 }
-                else if ((elevator1-curReq)*(elevator1-curReq) < (elevator2-curReq)*(elevator2-curReq)){
+                else if ((elevator1-curReq)*(elevator1-curReq) <= (elevator2-curReq)*(elevator2-curReq)){
                     System.out.println("1 " + curReq);
                     System.out.println(access);
                     elevator1 = curReq;
@@ -64,7 +99,7 @@ public class Elevator {
                     System.out.println("1 " + curReq);
                     elevator1 = curReq;
                 }
-                else if ((elevator1-curReq)*(elevator1-curReq) < (elevator2-curReq)*(elevator2-curReq)){
+                else if ((elevator1-curReq)*(elevator1-curReq) <= (elevator2-curReq)*(elevator2-curReq)){
                     System.out.println("1 " + curReq);
                     elevator1 = curReq;
                 }
@@ -74,7 +109,6 @@ public class Elevator {
                 }
             }
             requests /= 10;
-            count--;
         }
     }  
 }
